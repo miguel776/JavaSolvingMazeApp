779fc2e41a44aad87f8b1bfbda4f6f86397b7a85
miguel776 <miguelgonzalez776@gmail.com>
2019-09-08 17:42:09 +0200

    Update MazeJunifer.java

--- maze_solution_java/src/main/java/com/Junifer/mazes/MazeJunifer.java
+++ maze_solution_java/src/main/java/com/Junifer/mazes/MazeJunifer.java
@@ -1,46 +1,39 @@
 package com.Junifer.mazes;
 
-
 /** 
- *  - This interface is implemented  by the "OfferImpl" class and provide the functionalities of  Load the file.txt, Solve a maze 
- *    and Formatted the result to show in the console.
- * 
- * 
- */
-
-
+*  - This interface is implemented  by the "OfferImpl" class and provide the functionalities of  Load the file.txt, Solve a maze 
+*    and Formatted the result to show in the console.
+*/
 public interface MazeJunifer {
 	
-	
-	/**
+    /**
      *  Load the file.txt
      *
      * @param String file - Location of the file containing the input data.
      * @return A matrix "maze" that content the maze without solving.
      */
-	public  String[][] loadDataFile(String file);
+     public  String[][] loadDataFile(String file);
 	
-	/**
+    /**
      * Solve the maze
      *
      * @param  A matrix "maze" that content the maze without solving.
      * @return A matrix "maze" that content the maze solves.
      */
-	public  String[][] solvemazes(String[][] maze);
+     public  String[][] solvemazes(String[][] maze);
 		
-	/**
+    /**
      * Formatted the result to show in the console.
      *
      * @param  A matrix "maze" that content the maze solves.
      * @return A matrix "maze" that content the maze format.
      */
-	public  String[][] formatOUT(String[][] maze);
+     public  String[][] formatOUT(String[][] maze);
 	
-	/**
+    /**
      * It show the result in the console.
      *
      * @param A matrix "mazeFormats" that content the maze solves and Formatted. 
      */
-	public  void showMaze(String[][] maze);
-
+     public  void showMaze(String[][] maze);
 }
