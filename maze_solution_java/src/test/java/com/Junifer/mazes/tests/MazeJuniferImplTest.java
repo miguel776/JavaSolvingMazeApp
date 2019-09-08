db3b1a7d0cdc15a0b204f4c53a8fed00664a48f6
miguel776 <miguelgonzalez776@gmail.com>
2019-09-08 17:44:22 +0200

    Update MazeJuniferImplTest.java

--- maze_solution_java/src/test/java/com/Junifer/mazes/tests/MazeJuniferImplTest.java
+++ maze_solution_java/src/test/java/com/Junifer/mazes/tests/MazeJuniferImplTest.java
@@ -4,38 +4,31 @@ import org.junit.Test;
 import com.Junifer.mazes.MazeApp;
 import static org.junit.Assert.assertEquals;
 
-
 public class MazeJuniferImplTest {
 
     public MazeApp mazeApp; 
 
-
     //......................................................................
     //Check the result of the maze content in the file example.txt
     //......................................................................
-
     /**
      * - The correct functionality is checked according value of (x,y) location of the end
      * - It must be equal maze[8][8]="E", it will  indicated that there is a solution
-     * 
      */
     @Test
     public void testAppSolvingMaze_example() throws Exception {
     	
     	mazeApp = new MazeApp();
- 		mazeApp.appSolvingMaze("/example.txt");
- 		assertEquals(mazeApp.maze[8][8], "E");
-		
+ 	mazeApp.appSolvingMaze("/example.txt");
+ 	assertEquals(mazeApp.maze[8][8], "E");	
      }
 
     //......................................................................
     //Check the result of the maze content in the file small.txt
     //......................................................................
-
     /**
      * - The correct functionality is checked according value of (x,y) location of the end
      * - It must be equal maze[4][3]="E", it will  indicated that there is a solution
-     * 
      */
     @Test
     public void testAppSolvingMaze_small() throws Exception {
@@ -48,11 +41,9 @@ public class MazeJuniferImplTest {
     //......................................................................
     //Check the result of the maze content in the file small_noWayOut.txt
     //......................................................................
-
     /**
      * - The correct functionality is checked according value of (x,y) location of the end
-     * - It must be equal maze[4][3]="N", it will  indicated that there isn´t a solution
-     * 
+     * - It must be equal maze[4][3]="N", it will  indicated that there isn´t a solution 
      */
 
     @Test
@@ -61,13 +52,11 @@ public class MazeJuniferImplTest {
     	mazeApp = new MazeApp();
     	mazeApp.appSolvingMaze("/small_noWayOut.txt");
     	assertEquals(mazeApp.maze[4][3], "N");
-
-       }
+    }
 
     //......................................................................
     //Check the result of the maze content in the file sparse_medium.txt
     //......................................................................
-
     /**
      * - The correct functionality is checked according value of (x,y) location of the end
      * - It must be equal maze[19][19]="E", it will  indicated that there is a solution.
@@ -79,32 +68,27 @@ public class MazeJuniferImplTest {
     	mazeApp = new MazeApp();
     	mazeApp.appSolvingMaze("/sparse_medium.txt");
     	assertEquals(mazeApp.maze[19][19], "E");
-    	
     }
 
     //......................................................................
     //Check the result of the maze content in the file medium_input.txt
     //......................................................................
-
     /**
      * - The correct functionality is checked according value of (x,y) location of the end
      * - It must be equal maze[19][21]="E", it will  indicated that there is a solution
      *    
      */
-
     @Test
     public void testAppSolvingMaze_medium_input() throws Exception {
     	
     	mazeApp = new MazeApp();
     	mazeApp.appSolvingMaze("/medium_input.txt");
     	assertEquals(mazeApp.maze[19][21], "E");
-       
     }
 
     //......................................................................
     //Check the result of the maze content in the file input.txt
     //......................................................................
-
     /**
      * - The correct functionality is checked according value of (x,y) location of the end
      * - It must be equal maze[3][3]="E", it will  indicated that there is a solution
@@ -117,13 +101,11 @@ public class MazeJuniferImplTest {
     	mazeApp = new MazeApp();
     	mazeApp.appSolvingMaze("/input.txt");
     	assertEquals(mazeApp.maze[3][3], "E");
-      
     }
     
     //......................................................................
     //Check the result of the maze content in the file large_input.txt 
     //......................................................................
-
     /**
      * - The correct functionality is checked according value of (x,y) location of the end
      * - It must be equal maze[99][159]="E", it will  indicated that there is a solution
@@ -135,9 +117,6 @@ public class MazeJuniferImplTest {
     	
     	mazeApp = new MazeApp();
     	mazeApp.appSolvingMaze("/large_input.txt");
-    	assertEquals(mazeApp.maze[99][159], "E");
-      
+    	assertEquals(mazeApp.maze[99][159], "E"); 
     }
-    
-    
 }
