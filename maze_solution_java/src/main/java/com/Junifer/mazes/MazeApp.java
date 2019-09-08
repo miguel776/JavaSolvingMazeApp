c81dc819b17c2b4a8677d45a0ddd163a4b2fa5c8
miguel776 <miguelgonzalez776@gmail.com>
2019-09-08 17:40:32 +0200

    Update MazeApp.java

--- maze_solution_java/src/main/java/com/Junifer/mazes/MazeApp.java
+++ maze_solution_java/src/main/java/com/Junifer/mazes/MazeApp.java
@@ -1,39 +1,34 @@
 package com.Junifer.mazes;
 
+/**
+* - Service to provide the functionality to solve simple mazes. The mazes are given in 
+*   a file and the program is going to read in the file, solve the maze and output the solution. 
+*   If no solution is possible the output it will be  indicated.
+*   
+* - It use the interface mazeJunifer to execute three process to solved a maze.
+*    + Load the data of a file.txt that content the maze and the (x,y) location of the start and end.
+*    + Solve the maze and return the solution.
+*    + Formatted the result to show in the console.
+*    + Show the maze in console.
+* 
+*/
 public class MazeApp {
 	
+   private static MazeJunifer mazeJunifer = new MazeJuniferImpl();
+   public String[][] maze;
 	
-	/**
- 	* - Service to provide the functionality to solve simple mazes. The mazes are given in 
- 	*   a file and the program is going to read in the file, solve the maze and output the solution. 
- 	*   If no solution is possible the output it will be  indicated.
- 	*   
- 	* - It use the interface mazeJunifer to execute three process to solved a maze.
- 	*    + Load the data of a file.txt that content the maze and the (x,y) location of the start and end.
- 	*    + Solve the maze and return the solution.
- 	*    + Formatted the result to show in the console.
- 	*    + Show the maze in console.
- 	* 
- 	*/
+   public  void appSolvingMaze(String file){
 	
-	private static MazeJunifer mazeJunifer = new MazeJuniferImpl();
-	public String[][] maze;
-	
-	public  void appSolvingMaze(String file){
+	// Load the data of a file.txt that content the maze and the (x,y) location of the start and end ...
+        maze = mazeJunifer.loadDataFile(file); 
 		
+	// Solve the maze and return the solution ... 
+	maze = mazeJunifer.solvemazes(maze); 
 		
-		// Load the data of a file.txt that content the maze and the (x,y) location of the start and end ...
-		maze = mazeJunifer.loadDataFile(file); 
+	// Formatted the result to show in the console...
+	maze = mazeJunifer.formatOUT(maze);
 		
-		// Solve the maze and return the solution ... 
-		maze = mazeJunifer.solvemazes(maze); 
-		
-		// Formatted the result to show in the console...
-		maze = mazeJunifer.formatOUT(maze);
-		
-		// Show the maze in console...
-		mazeJunifer.showMaze(maze);	
-		
-		
-	}
+	// Show the maze in console...
+	mazeJunifer.showMaze(maze);	
+   }
 }
